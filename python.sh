11382b2829e8dec988348f804a33efa59d932d19
pawel koszalka <koszalix@gmail.com>
2023-07-22 20:01:56 +0200

    Auto update

--- python.sh
+++ python.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-pip3 install -r files/pip_requirements.txt --break-system-packages
+if [[ -e "${HOME}"/.config/pip/pip.conf ]] 
+then
+        mv "${HOME}"/.config/pip/pip.conf ~/.config/pip/pip.conf.bak
+else
+        mkdir -p "${HOME}"/.config/pip
+fi
+
+cp files/pip.conf "${HOME}"/.config/pip/
+
+
+pip3 install -r files/pip_requirements.txt 
 
